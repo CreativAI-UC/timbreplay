11ac51c1e8c56e0b484ffc2a75d82fb7eb5865ca
mandrade2 <mandrade2@uc.cl>
2020-07-08 23:16:01 -0400

    dataset points and to json script

---
+++ build-points-json.js
@@ -0,0 +1,18 @@
+// to be run in node
+var lineReader = require("readline").createInterface({
+  input: require("fs").createReadStream("points.txt"),
+})
+
+let result = []
+lineReader.on("line", function (line) {
+  let extracted_values = line.split("     ")
+  result.push({
+    name: extracted_values[1],
+    chord: extracted_values[1].split("_")[0],
+    values: JSON.parse(extracted_values[0].slice(1, -1).split(" ").join(",")),
+    dynamic: extracted_values[1].split("_")[1],
+    iteration: extracted_values[1].split("_")[2].replace(".wav",""),
+  })
+}).on("close", function(){
+    console.log(JSON.stringify(result, null, 4))
+})
